b58301e85f1d9faf1fa41627a54a2b29a548e936
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2024-02-04 06:36:58 +0300

    add model db, frontend/ create profile page

--- FrontEnd/src/View/ProfilePage.tsx
+++ FrontEnd/src/View/ProfilePage.tsx
@@ -1,9 +1,13 @@
-
+import styles from "./css/profile.module.css"
 export function ProfilePage() {
     
     return (
         <>
-            <h3>Hello, profile</h3>
+            <div className={styles.main__content}>
+                <div className={styles.profile__content}>
+                    <img src="https://rudos.ru/loadfiles/moduls/doska/bigfoto/4ba9a45a0f3bb49507c3c824769a9f7b.webp" alt="profile img" className={styles.profile__img}/>
+                </div>
+            </div>
         </>
     )
 }
