6e3b1088c6edc959e66ae1edd4943d31e65f2ec2
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2024-03-04 00:44:14 +0300

    add new element backend, rename folder with components auth

---
+++ BackEnd/controllers/interface/users.interface.ts
@@ -0,0 +1,11 @@
+interface Users {
+    user_id: number,
+    first_name: string,
+    last_name: string,
+    data_birth: number,
+    user_role: string,
+    email: string,
+    password: string;
+}
+
+export default Users;
